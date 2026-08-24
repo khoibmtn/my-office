ea66b1ed12abf90fde392dacba7b1341b104166f
Khoi Bui Minh <khoibm.tn@gmail.comm>
2026-08-24 18:57:43 +0700

    fix(search): add use client directive to search page to fix Vercel SSR build error

--- app/(app)/search/page.tsx
+++ app/(app)/search/page.tsx
@@ -1,7 +1,7 @@
-import { Suspense } from 'react'
-import { SearchClient } from '@/components/search/SearchClient'
+'use client'
 
-export const metadata = { title: 'Tìm kiếm' }
+import React, { Suspense } from 'react'
+import { SearchClient } from '@/components/search/SearchClient'
 
 export default function SearchPage() {
   return (
